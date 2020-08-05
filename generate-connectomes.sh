26f7b005fa20d0ee7b5f68d2fbcf4652daffb2e5
Brad Caron <bacaron@iu.edu>
2020-08-05 10:25:19 -0400

    Update generate-connectomes.sh

--- generate-connectomes.sh
+++ generate-connectomes.sh
@@ -6,15 +6,6 @@ set -e
 mkdir -p connectomes
 
 #### configurable parameters ####
-lmax2=`jq -r '.lmax2' config.json`
-lmax4=`jq -r '.lmax4' config.json`
-lmax6=`jq -r '.lmax6' config.json`
-lmax8=`jq -r '.lmax8' config.json`
-lmax10=`jq -r '.lmax10' config.json`
-lmax12=`jq -r '.lmax12' config.json`
-lmax14=`jq -r '.lmax14' config.json`
-lmax=`jq -r '.lmax' config.json`
-mask=`jq -r '.mask' config.json`
 ad=`jq -r '.ad' config.json`
 fa=`jq -r '.fa' config.json`
 md=`jq -r '.md' config.json`
@@ -39,19 +30,6 @@ else
 fi
 
 #### convert data to mif ####
-# fod
-if [ ! -f lmax${lmax}.mif ]; then
-	echo "converting fod"
-	fod=$(eval "echo \$lmax${lmax}")
-	mrconvert ${fod} lmax${lmax}.mif -force -nthreads ${ncores} -quiet
-fi
-
-# 5tt
-if [ ! -f 5tt.mif ]; then
-	echo "converting 5tt"
-	mrconvert ${mask} 5tt.mif -force -nthreads ${ncores} -quiet
-fi
-
 # parcellation
 if [ ! -f parc.mif ]; then
 	echo "converting parcellation"
@@ -70,13 +48,6 @@ do
 	fi
 done
 
-#### perform SIFT2 to identify streamline weights ####
-if [ ! -f weights.csv ]; then
-	echo "performing SIFT2 to identify streamlines weights"
-	tcksift2 ${track} lmax${lmax}.mif weights.csv -act 5tt.mif -out_mu mu.txt -fd_scale_gm -nthreads ${ncores} -force -quiet
-	mu=`cat mu.txt`
-fi
-
 #### generate connectomes ####
 # microstructure networks (if inputted)
 for MEAS in ${measures}
@@ -84,20 +55,20 @@ do
 	if [ ! -f ./connectomes/${MEAS}_mean.csv ]; then
 		echo "creating connectome for diffusion measure ${MEAS}"
 		tcksample ${track} ${MEAS}.mif mean_${MEAS}_per_streamline.csv -stat_tck mean -use_tdi_fraction -nthreads ${ncores} -force
-		tck2connectome ${track} parc.mif ./connectomes/${MEAS}_mean.csv -scale_file mean_${MEAS}_per_streamline.csv -tck_weights_in weights.csv -stat_edge mean -symmetric -zero_diagonal -nthreads ${ncores} -force
+		tck2connectome ${track} parc.mif ./connectomes/${MEAS}_mean.csv -scale_file mean_${MEAS}_per_streamline.csv -stat_edge mean -symmetric -zero_diagonal -nthreads ${ncores} -force
 	fi
 done
 
 # count network
 if [ ! -f ./connectomes/count.csv ]; then
 	echo "creating connectome for streamline count"
-	tck2connectome ${track} parc.mif ./connectomes/count.csv -tck_weights_in weights.csv -out_assignments assignments.csv -symmetric -zero_diagonal -force -nthreads ${ncores}
+	tck2connectome ${track} parc.mif ./connectomes/count.csv -out_assignments assignments.csv -symmetric -zero_diagonal -force -nthreads ${ncores}
 fi
 
 # length network
 if [ ! -f ./connectomes/length.csv ]; then
 	echo "creating connectome for streamline length"
-	tck2connectome ${track} parc.mif ./connectomes/length.csv -tck_weights_in weights.csv -scale_length -stat_edge mean -symmetric -zero_diagonal -force -nthreads ${ncores}
+	tck2connectome ${track} parc.mif ./connectomes/length.csv -scale_length -stat_edge mean -symmetric -zero_diagonal -force -nthreads ${ncores}
 fi
 
 if [ -f ./connectomes/count.csv ] && [ -f ./connectomes/length.csv ]; then
