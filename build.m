d68ff80f49a41776c01e1045e7df01d5c3e58860
Bradley Caron <bacaron@h2.carbonate.uits.iu.edu>
2020-08-04 23:09:59 -0400

    Reason: wrong repo

--- build.m
+++ build.m
@@ -1,4 +1,4 @@
-addpath(genpath('/N/u/hayashis/git/vistasoft'))
+addpath(genpath('/N/u/brainlife/git/vistasoft'))
 addpath(genpath('/N/u/brlife/git/jsonlab'))
 mcc -m -R -nodisplay -d compiled generateDensityConnectomes
 exit
--- compile.sh
+++ compile.sh
@@ -3,7 +3,7 @@ module load matlab/2017a
 mkdir -p compiled
 
 cat > build.m <<END
-addpath(genpath('/N/u/hayashis/git/vistasoft'))
+addpath(genpath('/N/u/brainlife/git/vistasoft'))
 addpath(genpath('/N/u/brlife/git/jsonlab'))
 mcc -m -R -nodisplay -d compiled generateDensityConnectomes
 exit
