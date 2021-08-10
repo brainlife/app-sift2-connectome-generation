020ce891da8b9b583b3135e3489359697e581727
Brad Caron <bacaron@iu.edu>
2021-08-09 20:44:10 -0400

    Update generate-connectomes.sh

--- generate-connectomes.sh
+++ generate-connectomes.sh
@@ -28,6 +28,7 @@ odi=`jq -r '.odi' config.json`
 isovf=`jq -r '.isovf' config.json`
 track=`jq -r '.track' config.json`
 parc=`jq -r '.parc' config.json`
+label=`jq -r '.label' config.json`
 #inflate=`jq -r '.lmax2' config.json`
 ncores=8
 
@@ -46,6 +47,14 @@ elif [ ! -f ${ndi} ] && [ -f ${ga} ]; then
 else
 	measures="ndi odi isovf"
 fi
+
+#### conmat measures ####
+conmat_measures="count density length denlen"
+
+for i in ${conmat_measures}
+do
+	mkdir -p ${i}_out ${i}_out/csv
+done
 
 #### convert data to mif ####
 # fod
@@ -95,7 +104,6 @@ do
 		tcksample ${track} ${MEAS}.mif mean_${MEAS}_per_streamline.csv -stat_tck mean -use_tdi_fraction -nthreads ${ncores} -force
 		tck2connectome ${track} parc.mif ./connectomes/${MEAS}_mean.csv -scale_file mean_${MEAS}_per_streamline.csv -tck_weights_in weights.csv -stat_edge mean -symmetric -zero_diagonal -nthreads ${ncores} -force
 		tck2connectome ${track} parc.mif ./connectomes/${MEAS}_mean_density.csv -scale_file mean_${MEAS}_per_streamline.csv -scale_invnodevol -tck_weights_in weights.csv -stat_edge mean -symmetric -zero_diagonal -nthreads ${ncores} -force
-
 	fi
 done
 
@@ -103,24 +111,36 @@ done
 if [ ! -f ./connectomes/count.csv ]; then
 	echo "creating connectome for streamline count"
 	tck2connectome ${track} parc.mif ./connectomes/count.csv -tck_weights_in weights.csv -out_assignments assignments.csv -symmetric -zero_diagonal -force -nthreads ${ncores}
+	cp ./connectomes/count.csv ./count_out/csv/correlation.csv
+	cp ${label} ./count_out/
+	cp ./templates/index.json ./count_out/
 fi
 
 # count density network
 if [ ! -f ./connectomes/density.csv ]; then
 	echo "creating connectome for streamline count"
 	tck2connectome ${track} parc.mif ./connectomes/density.csv -scale_invnodevol -tck_weights_in weights.csv -out_assignments assignments.csv -symmetric -zero_diagonal -force -nthreads ${ncores}
+	cp ./connectomes/density.csv ./density_out/csv/correlation.csv
+	cp ${label} ./density_out/
+	cp ./templates/index.json ./density_out/
 fi
 
 # length network
 if [ ! -f ./connectomes/length.csv ]; then
 	echo "creating connectome for streamline length"
 	tck2connectome ${track} parc.mif ./connectomes/length.csv -tck_weights_in weights.csv -scale_length -stat_edge mean -symmetric -zero_diagonal -force -nthreads ${ncores}
+	cp ./connectomes/length.csv ./length_out/csv/correlation.csv
+	cp ${label} ./length_out/
+	cp ./templates/index.json ./length_out/
 fi
 
 # density of length network
 if [ ! -f ./connectomes/denlen.csv ]; then
 	echo "creating connectome for streamline count"
 	tck2connectome ${track} parc.mif ./connectomes/denlen.csv -tck_weights_in weights.csv -scale_length -scale_invnodevol -out_assignments assignments.csv -symmetric -zero_diagonal -force -nthreads ${ncores}
+	cp ./connectomes/length.csv ./denlen_out/csv/correlation.csv
+	cp ${label} ./denlen_out/
+	cp ./templates/index.json ./denlen_out/
 fi
 
 # generate centers csv
@@ -142,6 +162,13 @@ if [ -f ./connectomes/count.csv ] && [ -f ./connectomes/length.csv ]; then
 			rm -rf tmp.csv
 		fi
 	done
+	for conmats in ${conmat_measures}
+	do
+		sed -e 's/\s\+/,/g' ./${conmats}_out/csv/correlation.csv > ./${conmats}_out/csv/tmp.csv
+		cat ./${conmats}_out/csv/tmp.csv > ./${conmats}_out/csv/correlation.csv
+		rm -rf ./${conmats}_out/csv/tmp.csv
+	done
+
 else
 	echo "something went wrong"
 fi
